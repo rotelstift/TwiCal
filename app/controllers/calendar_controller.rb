30d0e2f2bf9fbaae47bceb48ba435913f4dae249
rotelstift <rotelstift@gmail.com>
2016-09-28 16:29:17 +0900

    カレンダーを大体作った

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -4,17 +4,28 @@ class CalendarController < ApplicationController
   def home
 
     # 今日の日付を設定する
-    @now = Date.today
+    @display_time = Date.today
 
     # 月初めの日付と月終わりの日付を設定する。
-    @beginning_of_month = @now.beginning_of_month
-    @end_of_month = @now.end_of_month
+    @beginning_of_month = @display_time.beginning_of_month
+    @end_of_month = @display_time.end_of_month
 
     @calendar = set_calendar(@beginning_of_month, @end_of_month)
 
   end
 
-  def set_calendar(beginning, ending)
+  def calendar
+    # 表示したい日付を設定する
+    @display_time = Date.parse(params[:display_time])
+
+    # 月初めの日付と月終わりの日付を設定する。
+    @beginning_of_month = @display_time.beginning_of_month
+    @end_of_month = @display_time.end_of_month
+
+    @calendar = set_calendar(@beginning_of_month, @end_of_month)
+  end
+
+  private def set_calendar(beginning, ending)
     calendar_ary = Array.new
 
     start = beginning.beginning_of_week
