b60fe6391103703ddfff2c67ac39a6fc564c0ff2
rotelstift <rotelstift@gmail.com>
2017-04-11 20:27:44 +0900

    2重ループ解消のリファクタ成功

--- app/controllers/tweets_module.rb
+++ app/controllers/tweets_module.rb
@@ -5,6 +5,14 @@ module TweetsModule
 
   TWEETS_TO_GET = 100
 
+
+  # 今月分のツイートをとってくる関数
+  # リファクタリングしたい
+  #
+  # 方針としては、まず変則2重ループとなっている構造をなくす
+  # そのために、ループ1とループ2に分ける
+
+
   def tweets_in_this_month(display_time, current_user)
 
     # 今月のツイートを入れる配列。二次元配列になっていて、
@@ -23,17 +31,13 @@ module TweetsModule
     # それを読み込んでuser_timelineに渡したいなぁ。。。
     tweet_id = tweet_db.get_nearest_tweet(display_time, current_user.id)
 
-    # if tweet_id then
-    #   timeline = user_timeline(TWEETS_TO_GET, tweet_id)
-    # end
-
     # 読み込んだ中で最古のtweetの日付が表示したい日付の月初めより
     # 新しい場合にのみループする関数
     begin
       if timeline then
         # timeline.last.idだけを引数にすると、無限ループになる場合がある
         # timeline = user_timeline(TWEETS_TO_GET, (timeline.last.id.to_i - 1).to_s)
-        timeline = user_timeline(TWEETS_TO_GET, tweet_db.rounding_id(timeline.last.id))
+        timeline = timeline + user_timeline(TWEETS_TO_GET, tweet_db.rounding_id(timeline.last.id))
       elsif !tweet_id.blank?
         timeline = user_timeline(TWEETS_TO_GET, tweet_db.rounding_id(tweet_id))
       else
@@ -44,20 +48,23 @@ module TweetsModule
         p 'timeline is []'
         break
       end
-
-      timeline.each do |tweet|
-        if (tweet.created_at.year == display_time.year) && (tweet.created_at.month == display_time.month) then
-
-          tweets_in_this_month[tweet.created_at.day].push({
-            text:      Rinku.auto_link(tweet.full_text, :all, 'target="blank"'),
-            date_time: tweet.created_at,
-            screen_name: tweet.user.screen_name,
-            id:        tweet.id,
-            tweet_url: tweet.url.to_s
-          })
-        end
-      end
     end while (timeline.last.created_at >= display_time.beginning_of_month)
+
+
+
+    timeline.each do |tweet|
+      if (tweet.created_at.year == display_time.year) && (tweet.created_at.month == display_time.month) then
+
+        tweets_in_this_month[tweet.created_at.day].push({
+          text:      Rinku.auto_link(tweet.full_text, :all, 'target="blank"'),
+          date_time: tweet.created_at,
+          screen_name: tweet.user.screen_name,
+          id:        tweet.id,
+          tweet_url: tweet.url.to_s
+        })
+      end
+    end
+
 
     for i in 1..display_time.end_of_month.day do
       #binding.pry
