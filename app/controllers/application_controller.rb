f41fea267bb5efbcdff4722572598b5b2b60cc86
rotelstift <rotelstift@gmail.com>
2017-04-18 20:57:40 +0900

    API制限がかかった時の処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from Twitter::Error::TooManyRequests, with: :tweet_reach_limit
+
+  def tweet_reach_limit
+    render 'static_pages/tweet_reach_limit', status: 500
+  end
+
   def hello
     render text: "hello, world!"
   end
